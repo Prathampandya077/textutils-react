1fde79102f89121b716e511f7510c09cfbb2b08a
prathampandya <pandyap196@gmail.com>
2024-02-29 01:53:01 +0530

    Updates

---
+++ precache-manifest.b4e79e924d618d80ffbe3707cbd9f96f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc0c69a8633ab11b76cb7eebf16c7d4",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "db84779ac11fd3b8ddf7",
+    "url": "/textutils/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "8946a0edaa5c8cb07476",
+    "url": "/textutils/static/js/2.2a068b1b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/textutils/static/js/2.2a068b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/textutils/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "db84779ac11fd3b8ddf7",
+    "url": "/textutils/static/js/main.9f09ee49.chunk.js"
+  },
+  {
+    "revision": "79ea64f27fef7de3a5b2",
+    "url": "/textutils/static/js/runtime-main.d25e19fa.js"
+  }
+]);
